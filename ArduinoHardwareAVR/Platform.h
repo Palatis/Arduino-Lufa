8c91c29a37227c6c748986ed5d00dba054f94541
Victor Tseng <palatis@gmail.com>
2014-02-15 20:26:21 +0800

    Edited Arduino Core Files

---
+++ ArduinoHardwareAVR/Platform.h
@@ -0,0 +1,17 @@
+
+#ifndef __PLATFORM_H__
+#define __PLATFORM_H__
+
+#include <inttypes.h>
+#include <avr/pgmspace.h>
+#include <avr/eeprom.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned long u32;
+
+#include "Arduino.h"
+
+#endif
