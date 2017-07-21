64d7274352eec3072c89b00e6819eb8b64704a48
iFreilicht <felix.uhl@outlook.com>
2017-07-21 22:48:34 +0200

    Make hiding more robust

--- activate.py
+++ activate.py
@@ -42,92 +42,43 @@ SECTIONS_TO_HIDE_IN_FILES = {
         """)
 }
 
-#Text to be inserted at the top of blocked files
-BLOCK_HEADER = textwrap.dedent("""
-    //==========================================================
-    //== This file is blocked by LUFA and cannot be included! ==
-    //==========================================================
-    #error "LUFA is still activated! Please run `libraries/LUFA/deactivate.py' first!"
-""")
-
 #Text to be inserted above hidden sections in files
+#Because the file might contain multi-line comments, #if 0 ... #endif are used
 HIDE_HEADER = textwrap.dedent("""
     //=====================================
     //== This section is hidden by LUFA! ==
     //=====================================
-    /*
+    #if 0
 """)
 
 #Text to be inserted below hidden section in files
 #This contains additional text to make sure it is easily found when deactivating LUFA
 HIDE_FOOTER = textwrap.dedent("""
     END OF HIDDEN SECTION
-    */
+    #endif
 """)
-
-#----------#
-# Blocking #
-#----------#
-def block_file(path):
-    """Prevent file from being included"""
-
-    file_content = ""
-
-    with open(path, 'r') as original:
-        file_content = original.read()
-
-    # Check if there already is a bloc header
-    if file_content.find(BLOCK_HEADER) != -1:
-        print(("INFO: Block header already present in file `{}'. "
-               + 'File was left untouched.').format(path))
-        return
-
-    # Prepend block header
-    with open(path, 'w') as blocked:
-        blocked.write(BLOCK_HEADER + file_content)
-
-    print("Successfully blocked file `{}'!".format(path))
-
-def unblock_file(path):
-    """Allow previously blocked file to be included again"""
-
-    file_content = ""
-
-    with open(path, 'r') as original:
-        file_content = original.read()
-
-    block_header_start = file_content.find(BLOCK_HEADER)
-    if block_header_start == -1:
-        print(("WARNING: Block header not found in file `{}'. "
-               + 'File was left untouched.').format(path))
-        return
-
-    # Remove block header
-    cropped_content = file_content[0 : block_header_start] \
-        + file_content[block_header_start + len(BLOCK_HEADER) : len(file_content)]
-
-    with open(path, 'w') as unblocked:
-        unblocked.write(cropped_content)
-
-    print("Successfully unblocked file `{}'!".format(path))
 
 #--------#
 # Hiding #
 #--------#
+def hide_whole_file(path):
+    """Hide whole file"""
+    hide_section_in_file(path, False)
+
+def unhide_whole_file(path):
+    """Make previously hidden file available again"""
+    unhide_section_in_file(path)
+
 def hide_section_in_file(path, section):
-    """Hide section by surrounding it with comment tags"""
+    """
+    Hide section
+    If section is a falsey value, the whole file will be hidden
+    """
 
     file_content = ""
 
     with open(path, 'r') as original:
         file_content = original.read()
-
-    section_start = file_content.find(section)
-    if section_start == -1:
-        print(("WARNING: Section below not found in file `{}`. "
-               + 'File was left untouched. Section:\n{}\n')
-              .format(path, section))
-        return
 
     # Check if header or footer are already present
     if file_content.find(HIDE_HEADER) != -1 \
@@ -136,17 +87,35 @@ def hide_section_in_file(path, section):
                + 'File was left untouched.').format(path))
         return
 
+    # Prepare insertion indices
+    if section:
+        section_length = len(section)
+        section_start = file_content.find(section)
+        if section_start == -1:
+            print(("WARNING: Section below not found in file `{}`. "
+                   + 'File was left untouched. Section:\n{}\n')
+                  .format(path, section))
+            return
+    else:
+        # Comment out whole file if section is falsey
+        section_length = len(file_content)
+        section_start = 0
+
     # Insert header before and footer after the section to hide
     modified_content = file_content[0 : section_start]                    \
                        + HIDE_HEADER                                      \
-                       + file_content[section_start : section_start + len(section)]\
+                       + file_content[section_start : section_start + section_length]\
                        + HIDE_FOOTER                                      \
-                       + file_content[section_start + len(section) : len(file_content)]\
+                       + file_content[section_start + section_length : len(file_content)]\
 
     with open(path, 'w') as modified:
         modified.write(modified_content)
 
-    print("Successfully hid section in file `{}'!".format(path))
+    # Report whether whole file or just a section has been hidden
+    if not section:
+        print("Successfully hid file `{}'!".format(path))
+    else:
+        print("Successfully hid section in file `{}'!".format(path))
 
 def unhide_section_in_file(path):
     """Make previously hidden section available again"""
@@ -158,11 +127,14 @@ def unhide_section_in_file(path):
 
     header_start = file_content.find(HIDE_HEADER)
     footer_start = file_content.find(HIDE_FOOTER)
+
+    # Check if header and footer were found
     if header_start == -1 or footer_start == -1:
         print(("WARNING: Hide header and/or footer not found in file `{}'. "
                + 'File was left untouched.').format(path))
         return
 
+    # Check if header appears before footer
     if header_start > footer_start:
         print(("WARNING: Hide footer found before header in file `{}'. "
                + 'File was left untouched, it requires manual attention!')
@@ -215,9 +187,9 @@ def main_common(block_and_hide):
     for file_to_block in FILES_TO_BLOCK:
         path = ARDUINO_CORE_DIR + file_to_block
         if block_and_hide:
-            block_file(path)
+            hide_whole_file(path)
         else:
-            unblock_file(path)
+            unhide_whole_file(path)
 
     for in_file, section in SECTIONS_TO_HIDE_IN_FILES.items():
         path = ARDUINO_CORE_DIR + in_file
